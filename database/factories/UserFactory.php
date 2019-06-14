a3e5e2b49bbc0636031b4eb86c6c42d87c078afe
solofeed <e.kuzmenko@synergetica.net>
2019-06-14 07:55:54 +0300

    Make user auth, update laravel to 5.8

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,8 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +17,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'name'           => $faker->name,
+        'email'          => 'admin@admin.com',
+        'password'       => Hash::make('password'),
+        'remember_token' => Str::random(10),
     ];
 });
